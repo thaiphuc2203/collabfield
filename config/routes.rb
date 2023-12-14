ce86608f3729b863044c10faa95c5bc86782926f
thaiphuc2203 <thaiphuc2203@gmail.com>
2023-12-14 23:29:26 +0700

    add new function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'errors/not_found'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
   get 'up' => 'rails/health#show', as: :rails_health_check
   root to: 'pages#index'
